e8dad00b587e1ca71f824a2c43949ef4f3de335f
Iretibe <yessouf2009@yahoo.fr>
2021-12-12 17:07:30 +0000

    Latest

--- SatoshiApp.IdentityServer/Config.cs
+++ SatoshiApp.IdentityServer/Config.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 
 namespace SatoshiApp.IdentityServer
@@ -12,47 +14,260 @@ namespace SatoshiApp.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
+                       new IdentityResources.Email(),
+                        new IdentityResources.OpenId(),
+                        new IdentityResources.Profile()
                    };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("satoshiapi", "Satoshi System API",new List<string> { "role" })
+                {
+                    ApiSecrets = { new Secret("FGK$DEF1487jh&^$shcbDIF%".Sha256()) }
+                }
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("satoshiapi", "Satoshi API")
             };
 
-        public static IEnumerable<Client> Clients =>
+        public static IEnumerable<Client> Clients(IConfiguration configuration) =>
             new Client[]
             {
-                // m2m client credentials flow client
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
+                    ClientId = "customerapicode",
+                    ClientName = "Customer API Code",
                     AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
+                    AlwaysIncludeUserClaimsInIdToken = false,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    AllowRememberConsent = true,
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    RedirectUris = new []
+                    {
+                        "https://localhost:44330/swagger/oauth2-redirect.html",
+                        "https://localhost:44330/oauth2-redirect.html",
+                        "http://localhost:9002/swagger/oauth2-redirect.html",
+                        "http://localhost:9002/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiCustomerApi/swagger/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiCustomerApi/oauth2-redirect.html",
+                        "http://localhost/SatoshiCustomerApi/swagger/oauth2-redirect.html",
+                        "http://localhost/SatoshiCustomerApi/oauth2-redirect.html"
+                    },
+                    PostLogoutRedirectUris = new []
+                    {
+                        "https://localhost:44330/swagger/signout-callback-oidc",
+                        "https://localhost:44330/signout-callback-oidc",
+                        "http://localhost:9002/swagger/signout-callback-oidc",
+                        "http://localhost:9002/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiCustomerApi/swagger/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiCustomerApi/signout-callback-oidc",
+                        "http://localhost/SatoshiCustomerApi/swagger/signout-callback-oidc",
+                        "http://localhost/SatoshiCustomerApi/signout-callback-oidc"
+                    },
+                    AllowedCorsOrigins = new []
+                    {
+                        "https://localhost:44330",
+                        "http://localhost:9002",
+                        "http://codelearnersoft.net",
+                        "http://localhost"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "satoshiapi"
+                    }
                 },
+
+                new Client
+                {
+                    ClientId = "productapicode",
+                    ClientName = "Product API Code",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AlwaysIncludeUserClaimsInIdToken = false,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    AllowRememberConsent = true,
+                    AllowOfflineAccess = true,
+                    RedirectUris = new []
+                    {
+                        "https://localhost:49153/swagger/oauth2-redirect.html",
+                        "https://localhost:49153/oauth2-redirect.html",
+                        "http://localhost:9003/swagger/oauth2-redirect.html",
+                        "http://localhost:9003/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiProductApi/swagger/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiProductApi/oauth2-redirect.html",
+                        "http://localhost/SatoshiProductApi/swagger/oauth2-redirect.html",
+                        "http://localhost/SatoshiProductApi/oauth2-redirect.html"
+                    },
+                    PostLogoutRedirectUris = new []
+                    {
+                        "https://localhost:49153/swagger/signout-callback-oidc",
+                        "https://localhost:49153/signout-callback-oidc",
+                        "http://localhost:9003/swagger/signout-callback-oidc",
+                        "http://localhost:9003/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiProductApi/swagger/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiProductApi/signout-callback-oidc",
+                        "http://localhost/SatoshiProductApi/swagger/signout-callback-oidc",
+                        "http://localhost/SatoshiProductApi/signout-callback-oidc"
+                    },
+                    AllowedCorsOrigins = new []
+                    {
+                        "https://localhost:49153",
+                        "http://localhost:9003",
+                        "http://codelearnersoft.net",
+                        "http://localhost"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "satoshiapi"
+                    }
+                },
+
+                new Client
+                {
+                    ClientId = "orderapicode",
+                    ClientName = "Order API Code",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AlwaysIncludeUserClaimsInIdToken = false,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    AllowRememberConsent = true,
+                    AllowOfflineAccess = true,
+                    RedirectUris = new []
+                    {
+                        "https://localhost:44396/swagger/oauth2-redirect.html",
+                        "https://localhost:44396/oauth2-redirect.html",
+                        "http://localhost:9004/swagger/oauth2-redirect.html",
+                        "http://localhost:9004/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiOrderApi/swagger/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiOrderApi/oauth2-redirect.html",
+                        "http://localhost/SatoshiOrderApi/swagger/oauth2-redirect.html",
+                        "http://localhost/SatoshiOrderApi/oauth2-redirect.html"
+                    },
+                    PostLogoutRedirectUris = new []
+                    {
+                        "https://localhost:44396/swagger/signout-callback-oidc",
+                        "https://localhost:44396/signout-callback-oidc",
+                        "http://localhost:9004/swagger/signout-callback-oidc",
+                        "http://localhost:9004/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiOrderApi/swagger/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiOrderApi/signout-callback-oidc",
+                        "http://localhost/SatoshiOrderApi/swagger/signout-callback-oidc",
+                        "http://localhost/SatoshiOrderApi/signout-callback-oidc"
+                    },
+                    AllowedCorsOrigins = new []
+                    {
+                        "https://localhost:44396",
+                        "http://localhost:9004",
+                        "http://codelearnersoft.net",
+                        "http://localhost"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "satoshiapi"
+                    }
+                },
+
+                new Client
+                {
+                    ClientId = "userapicode",
+                    ClientName = "User API Code",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AlwaysIncludeUserClaimsInIdToken = false,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    AllowRememberConsent = true,
+                    AllowOfflineAccess = true,
+                    RedirectUris = new []
+                    {
+                        "https://localhost:44375/swagger/oauth2-redirect.html",
+                        "https://localhost:44375/oauth2-redirect.html",
+                        "http://localhost:9001/swagger/oauth2-redirect.html",
+                        "http://localhost:9001/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiOrderApi/swagger/oauth2-redirect.html",
+                        "http://codelearnersoft.net/SatoshiOrderApi/oauth2-redirect.html",
+                        "http://localhost/SatoshiOrderApi/swagger/oauth2-redirect.html",
+                        "http://localhost/SatoshiOrderApi/oauth2-redirect.html"
+                    },
+                    PostLogoutRedirectUris = new []
+                    {
+                        "https://localhost:44375/swagger/signout-callback-oidc",
+                        "https://localhost:44375/signout-callback-oidc",
+                        "http://localhost:9001/swagger/signout-callback-oidc",
+                        "http://localhost:9001/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiUserApi/swagger/signout-callback-oidc",
+                        "http://codelearnersoft.net/SatoshiUserApi/signout-callback-oidc",
+                        "http://localhost/SatoshiUserApi/swagger/signout-callback-oidc",
+                        "http://localhost/SatoshiUserApi/signout-callback-oidc"
+                    },
+                    AllowedCorsOrigins = new []
+                    {
+                        "https://localhost:44375",
+                        "http://localhost:9001",
+                        "http://codelearnersoft.net",
+                        "http://localhost"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "satoshiapi"
+                    }
+                },
+
+                new Client
+                {
+                    ClientId = "satoshiuicode",
+                    ClientName = "Satoshi UI Code",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AlwaysIncludeUserClaimsInIdToken = false,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    AllowRememberConsent = true,
+                    AllowOfflineAccess = true,
+                    RedirectUris = new []
+                    {
+                        "https://localhost:44315/dashboard",
+                        "https://localhost:9005/dashboard",
+                        "http://codelearnersoft.net/SatoshiUI/dashboard",
+                        "http://localhost/SatoshiUI/swagger/dashboard"
+                    },
+                    PostLogoutRedirectUris = new []
+                    {
+                        "https://localhost:44315/home",
+                        "https://localhost:9005/home",
+                        "http://codelearnersoft.net/SatoshiUI/home",
+                        "http://localhost/SatoshiUI/swagger/home"
+                    },
+                    AllowedCorsOrigins = new []
+                    {
+                        "https://localhost:44315",
+                        "https://localhost:9005",
+                        "http://codelearnersoft.net",
+                        "http://localhost"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "satoshiapi"
+                    }
+                }
             };
     }
 }
--- SatoshiApp.IdentityServer/Startup.cs
+++ SatoshiApp.IdentityServer/Startup.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-using IdentityServer4;
-using SatoshiApp.IdentityServer.Data;
-using SatoshiApp.IdentityServer.Models;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -12,6 +10,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using SatoshiApp.IdentityServer.Data;
+using SatoshiApp.IdentityServer.Models;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SatoshiApp.IdentityServer
 {
@@ -30,12 +35,19 @@ namespace SatoshiApp.IdentityServer
         {
             services.AddControllersWithViews();
 
+            IdentityModelEventSource.ShowPII = true;
+
+            services.AddSameSiteCookiePolicy();
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityServerConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            var connectionString = Configuration.GetConnectionString("IdentityServerConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
             var builder = services.AddIdentityServer(options =>
             {
@@ -47,44 +59,119 @@ namespace SatoshiApp.IdentityServer
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddAspNetIdentity<ApplicationUser>();
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
 
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
+                // this enables automatic token cleanup. this is optional.
+                options.EnableTokenCleanup = true;
+            })
+            .AddAspNetIdentity<ApplicationUser>();
+            //.AddProfileService<ProfileService>();
 
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            //Certificate
+            var cert = new X509Certificate2(Path.Combine(Environment.ContentRootPath, "idsrv3test.pfx"), "idsrv3test");
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                builder.AddSigningCredential(cert);
+            }
+
+            //services.AddScoped<IProfileService, ProfileService>();
+            //services.AddScoped<IUserService, UserService>();
+
+            // configures the OpenIdConnect handlers to persist the state parameter into the server-side IDistributedCache.
+            services.AddOidcStateDataFormatterCache();
+
+            services.AddAuthentication();
+                //.AddGoogle(options =>
+                //{
+                //    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
                     
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
+                //    // register your IdentityServer with Google at https://console.developers.google.com
+                //    // enable the Google+ API
+                //    // set the redirect URI to https://localhost:5001/signin-google
+                //    options.ClientId = "copy client ID from Google here";
+                //    options.ClientSecret = "copy client secret from Google here";
+                //});
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCookiePolicy();
+
+            // this will do the initial DB population
+            bool seed = Configuration.GetSection("Data").GetValue<bool>("Seed");
+            if (seed)
+            {
+                InitializeDatabase(app);
+                //throw new Exception("Seeding completed. Disable the seed flag in appsettings");
+            }
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                //app.UseDatabaseErrorPage();
             }
 
             app.UseStaticFiles();
 
             app.UseRouting();
+
             app.UseIdentityServer();
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
             });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients(Configuration))
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.ApiResources)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
