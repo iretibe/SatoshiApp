f9191512bfcf54f79d85d939296c990378e2ad82
Somad Yessoufou <yessouf2009@yahoo.fr>
2021-12-14 23:59:19 +0000

    get latest

---
+++ SatoshiApp.CustomerApi/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace SatoshiApp.CustomerApi.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+}
