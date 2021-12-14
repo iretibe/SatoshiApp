efc88b9798246edd376056537ca5a4d483f2408f
Somad <iretibe@users.noreply.github.com>
2021-12-14 18:23:05 +0000

    Data

---
+++ APIs/OrderApi/SatoshiApp.OrderApi.Infrastructure/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SatoshiApp.OrderApi.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatoshiApp.OrderApi.Infrastructure
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+
+            optionsBuilder.UseSqlServer("Server=codelearnersoft.net;Database=SatoshiDB;User Id=sa;Password=YEso!@12;Trusted_Connection=False;MultipleActiveResultSets=True;TrustServerCertificate=True");
+
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
